7912495c207604c7b7197a3e509110ec2d8f423a
sunny <sunny@ladigital.me>
2022-12-15 13:55:00 -0500

    changes: - updated database url

--- model/database.js
+++ model/database.js
@@ -1,6 +1,7 @@
 let mongoose = require("mongoose");
 // const server = "127.0.0.1";
-const server = "sunny:123@cluster0.yytcifj.mongodb.net";
+
+const server = "surya:123@cluster0.yqqus1n.mongodb.net";
 // mongodb+srv://sunny:<password>@cluster0.u57lf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const DatabaseName = "librabry-managmnet";
 // const url = 'mongodb+srv://<username>:<password>@cluster0.yytcifj.mongodb.net/?retryWrites=true&w=majority'
